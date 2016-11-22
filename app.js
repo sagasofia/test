2a1cea0bd9baebc67e486a86cc2423fd0e3124ca
sagasofia <slundquist@Saga-lundqvist-MacBook-Pro.local>
2016-11-21 23:59:56 -0500

    drop down

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
 $(document).ready(function () {
- 
- var count = 0;
-
- 	$('#a10').click(function () {
-	count = count + 10;
-	$('#out').html(count);
-
-	})
-
-
-    $(document).ready(function() {
-        $('.arrow').click(function() {
-            window.location.href = this.id + 'work2.html';
-        });
-    });
-
-
+	// console.logs the window.width
+	$(window).resize(function () {
+		var width = $(window).width()
+		
+		if (width >= '880px') {
+			$('.small-links').hide()
+		}
 })
-// 2. Add .click() event handlers for each of the boxes:
-// - A) boxes #a10, #a20, and #a30 should each add 10, 20, and 30, respectively, to the value in the center box, #out
-// - B) boxes #n10, #n20, and #n30 should each subtract 10, 20, and 30, respectively, from the value in the center box, #out
-// - C) #red and #blue should change the background color of #out to red and blue, respectively
-// - D) #out should change the background of #out to white, and set the value back to zero
-// HINT: define a variable, var count = 0, at the top of your function; use this variable to keep track of what to display inside of #out
-// Ex: when the user clicks #a10, add 10 to the var count, and then reflect this change in the HTML
-//
-// jQuery Methods Needed:
-// - .click()
-// - . text() or .html()
-// - .css()
+
+	$('.sausage').click(function () {
+	$('.small-links').slideToggle(1000);
+
+})	
+	})
